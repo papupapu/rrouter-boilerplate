7864c3f67d69758c684b53337c3e6be9a39fa841
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2026-01-29 12:45:08 +0100

    Type home route loader with FetchResponse<HomeData>
    - Add explicit return type to loader function
    - Import FetchResponse and HomeData types
    - Remove unused data prop pass-through to Home component
    - Enables type checking for loader error handling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/layout";
 
+import type { FetchResponse } from "../utils/errorTypes";
+import type { HomeData } from "../services/home";
 import * as homeServices from "../services/home";
 
 import { Home } from "../views/home/home";
@@ -11,12 +13,12 @@ export function meta() {
   ];
 }
 
-export async function loader() {
+export async function loader(): Promise<FetchResponse<HomeData>> {
   const product = await homeServices.fetchHomeData();
   return product;
 }
 
 export default function HomeRoute({ loaderData }: Route.ComponentProps) {
   console.log("Loader Data:", loaderData);
-  return <Home data={loaderData} />;
+  return <Home />;
 }
